7cab6ada0ea5b9c92d9e33d26f64521c23f5ba91
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-05-15 09:29:24 +0530

    Run code through CMD use mvn test -PSmoke code

---
+++ src/main/java/DibyanshVerma/Demo/App.java
@@ -0,0 +1,13 @@
+package DibyanshVerma.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/DibyanshVerma/Demo/Test1.java
@@ -0,0 +1,96 @@
+package DibyanshVerma.Demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	@Test
+	public void openBrowser() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		// Get the current time stamp
+//		long currentTimeMillis = System.currentTimeMillis();
+//
+//		String timestamp = String.valueOf(currentTimeMillis);
+//		String time = timestamp.replace(":", "").replace(" ", "_");
+//
+//		// Define the file path where you want to store the console output
+//		String filePath = "C:\\Users\\dibya\\OneDrive\\Documents\\OneNote Notebooks\\Console Output\\console_output_" + time + ".txt";
+//
+//		// Redirect System.out to the file
+//		try {
+//			File file = new File(filePath);
+//			FileOutputStream fos = new FileOutputStream(file);
+//			PrintStream ps = new PrintStream(fos);
+//			System.setOut(ps);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+
+		try {
+			// Define an array of username-password pairs
+			String[][] credentials = {
+
+					{ "911317101", "Test@1234" }, // 911317101:AFNMFX
+					{ "911317102", "Test@1234" }, // 911317102:AFNMFX
+//					
+					// Add more username-password pairs as needed
+			};
+
+			// Loop through credentials and perform logins
+			for (String[] credential : credentials) {
+				String username = credential[0];
+				String password = credential[1];
+
+				// Below Command to put the URL:
+				driver.get("https://pp.lifeatworkportal.com/countyofla");
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+				// Log In Page Command
+				// Find the UserName and password input fields and enter credentials
+				WebElement usernameInput = driver.findElement(By.id("USERNAME"));
+				usernameInput.sendKeys(username);
+				WebElement passwordInput = driver.findElement(By.id("password"));
+				passwordInput.sendKeys(password);
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("showhide")));
+				driver.findElement(By.id("showhide")).click();
+				Thread.sleep(2000);
+				driver.findElement(By.id("ContinueBtn")).click();
+
+				// Authorization Page Command
+				driver.findElement(By.id("CancelBtn")).click();
+
+				// Click on Site Again Link Command
+				driver.findElement(By.cssSelector("a[href='/countyofla']")).click();
+
+				System.out.println("" + username + " is Web Enabled!!!");
+
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+
+		}
+
+		finally {
+
+			// Close the browser session
+			driver.quit();
+		}
+
+	}
+
+}
